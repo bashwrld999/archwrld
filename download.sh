146ed4cf31ddb3002fda21ffa1702c61cef69102
bashwrld999 <bashwrld999@proton.me>
2025-03-29 20:39:37 +0100

    12

--- download.sh
+++ download.sh
@@ -10,9 +10,7 @@ ARTIFACT="archwrld-${BRANCH}"
 if [ -n "$HASH" ]; then
   curl -sL -o "${ARTIFACT}.zip" "https://github.com/${GITHUB_USER}/archwrld/archive/${HASH}.zip"
   bsdtar -x -f "${ARTIFACT}.zip"
-  cp -R "${ARTIFACT}"/*.sh "${ARTIFACT}"/*.conf "${ARTIFACT}"/files/ "${ARTIFACT}"/configs/ ./
 else
   curl -sL -o "${ARTIFACT}.zip" "https://github.com/${GITHUB_USER}/archwrld/archive/refs/heads/${BRANCH}.zip"
   bsdtar -x -f "${ARTIFACT}.zip"
-  cp -R "${ARTIFACT}"/*.sh "${ARTIFACT}"/*.conf "${ARTIFACT}"/files/ "${ARTIFACT}"/configs/ ./
 fi
