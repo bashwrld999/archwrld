19f90f7a0f55dec456a176f3c61c8bea2f6b3830
bashwrld999 <bashwrld999@proton.me>
2025-03-29 20:36:02 +0100

    11

--- download.sh
+++ download.sh
@@ -4,5 +4,15 @@ set -euo pipefail
 
 GITHUB_USER="bashwrld999"
 BRANCH="main"
+HASH=""
+ARTIFACT="archwrld-${BRANCH}"
 
-pacman -Syu --noconfirm --needed git
+if [ -n "$HASH" ]; then
+  curl -sL -o "${ARTIFACT}.zip" "https://github.com/${GITHUB_USER}/archwrld/archive/${HASH}.zip"
+  bsdtar -x -f "${ARTIFACT}.zip"
+  cp -R "${ARTIFACT}"/*.sh "${ARTIFACT}"/*.conf "${ARTIFACT}"/files/ "${ARTIFACT}"/configs/ ./
+else
+  curl -sL -o "${ARTIFACT}.zip" "https://github.com/${GITHUB_USER}/archwrld/archive/refs/heads/${BRANCH}.zip"
+  bsdtar -x -f "${ARTIFACT}.zip"
+  cp -R "${ARTIFACT}"/*.sh "${ARTIFACT}"/*.conf "${ARTIFACT}"/files/ "${ARTIFACT}"/configs/ ./
+fi
