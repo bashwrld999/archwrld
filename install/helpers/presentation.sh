822e21e39cd1350da4fb30e069cff93ab6df9859
bashwrld999 <bashwrld999@proton.me>
2025-10-01 16:58:57 +0200

    changed gum color

--- install/helpers/presentation.sh
+++ install/helpers/presentation.sh
@@ -44,5 +44,5 @@ export GUM_CONFIRM_PADDING="$PADDING"
 
 clear_logo() {
   printf "\033[H\033[2J" # Clear screen and move cursor to top-left
-  gum style --foreground 2 --padding "1 0 0 $PADDING_LEFT" "$(<"$LOGO_PATH")"
+  gum style --foreground 203 --padding "1 0 0 $PADDING_LEFT" "$(<"$LOGO_PATH")"
 }
