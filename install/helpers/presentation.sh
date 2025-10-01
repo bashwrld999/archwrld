9e65fdc1632df02ef16fe1a42f03c845fe4c9a3a
bashwrld999 <bashwrld999@proton.me>
2025-10-01 16:59:21 +0200

    Changed gum color

--- install/helpers/presentation.sh
+++ install/helpers/presentation.sh
@@ -31,7 +31,7 @@ export PADDING_LEFT_SPACES=$(printf "%*s" $PADDING_LEFT "")
 # Tokyo Night theme for gum confirm
 export GUM_CONFIRM_PROMPT_FOREGROUND="6"     # Cyan for prompt
 export GUM_CONFIRM_SELECTED_FOREGROUND="0"   # Black text on selected
-export GUM_CONFIRM_SELECTED_BACKGROUND="2"   # Green background for selected
+export GUM_CONFIRM_SELECTED_BACKGROUND="203"   # Green background for selected
 export GUM_CONFIRM_UNSELECTED_FOREGROUND="7" # White for unselected
 export GUM_CONFIRM_UNSELECTED_BACKGROUND="0" # Black background for unselected
 export PADDING="0 0 0 $PADDING_LEFT"         # Gum Style
